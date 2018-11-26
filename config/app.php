c70eb2b5af882f8a8c355f7fb32bbc60692ea6c1
Rafael Barros <rafaelbarros.df@gmail.com>
2018-11-26 20:17:11 -0200

    Criando aplicação backend

--- config/app.php
+++ config/app.php
@@ -164,7 +164,10 @@ return [
 
         /*
          * Package Service Providers...
+         *
          */
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Application Service Providers...
