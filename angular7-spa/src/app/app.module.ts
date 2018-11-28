44323a6e5e285290d83cf9b30d2f5fac319aacf7
Rafael Barros <rafaelbarros.df@gmail.com>
2018-11-27 23:18:08 -0200

    Criando componente de login

--- angular7-spa/src/app/app.module.ts
+++ angular7-spa/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
